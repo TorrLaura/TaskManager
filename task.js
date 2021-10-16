f059c547991432fb158ac412d6a717385900eda9
“TorrLaura” <laplebe35@gmail.com>
2021-10-16 15:14:56 -0500

    Created task.js file. Created starting variables for project.

---
+++ task.js
@@ -0,0 +1,14 @@
+// TASKMANAGER PROJECT
+
+const enterOpText = `What would you like to do (Please enter one fo the options below:)`
+
+// for restarting the TaskManager
+let restartTask = true;
+
+// For storing the range of options
+let rangeOp;
+
+// For storing the option chosen
+let randomOp
+
+// 
