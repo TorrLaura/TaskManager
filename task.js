ac00a05956c8c6bbb0ce558f9586143c22ca3e03
“TorrLaura” <laplebe35@gmail.com>
2021-10-18 12:10:32 -0500

    Changed starting message in task.js file

--- task.js
+++ task.js
@@ -1,7 +1,5 @@
 // TASKMANAGER PROJECT
 
-const enterOpText = `What would you like to do (Please enter one fo the options below:)`
-
 // for restarting the TaskManager
 let restartTask = true;
 
@@ -11,4 +9,17 @@ let rangeOp;
 // For storing the option chosen
 let randomOp
 
-// 
+const start = `What would you like to do (Please enter one of the options below):
+"TASKS - Display All Tasks"
+"NEW" - Add a New Task
+"Remove" - Remove a Task
+"CLOSE" Close the Task Manager`
+
+// Starting Task msg
+
+alert (start);
+
+
+
+
+
