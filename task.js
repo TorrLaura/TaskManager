54714e8249bf0efe9bb65b3208ffbcc0680db2bb
“TorrLaura” <laplebe35@gmail.com>
2021-10-19 10:21:24 -0500

    Setup recurring menu feature as long as something other than close is entered.

--- task.js
+++ task.js
@@ -9,8 +9,20 @@ What would you like to do (Please enter one of the options below):
 "CLOSE" - Close the Task Manager
 `;
 
-let userInput = prompt (menu);
+let userInput = prompt(menu);
 
+
+// Loops and continues to display until the user enters close
+while (userInput !== `CLOSE`) {
+
+    
+    
+    // Displays the menu again
+    userInput = prompt (menu);
+}
+
+// Alerts the user that they have closed the program
+alert (`Thank you for using Task Manager`)
 
 
 
