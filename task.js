4d1566a435b319bc8cc77c0dc004682f1bac31a1
“TorrLaura” <laplebe35@gmail.com>
2021-10-19 10:02:02 -0500

    Created setup menu.

--- task.js
+++ task.js
@@ -1,25 +1,19 @@
-// TASKMANAGER PROJECT
+// TASK MANAGER
 
-// for restarting the TaskManager
-const enter = 
-
-// For storing the range of options
-let rangeOp;
-
-// For storing the option chosen
-let randomOp
-
-const start = `TASK MANAGER
+const menu = `TASK MANAGER
 
 What would you like to do (Please enter one of the options below):
 "TASKS - Display All Tasks"
 "NEW" - Add a New Task
-"Remove" - Remove a Task
-"CLOSE" Close the Task Manager`
+"REMOVE" - Remove a Task
+"CLOSE" - Close the Task Manager
+`;
 
-// Starting Task msg
+let userInput = prompt (menu);
 
-alert (start);
+
+
+
 
 
 
